c62f73e8b8ea81fe232b27895de9a69e47ed618c
Brendan O'Dea <brendan.ode4@gmail.com>
2017-11-18 18:32:22 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,3 +2,9 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
+
+IcebreakerResponse.BatchCreate = function(emails) {
+  return emails.map(function(input){
+    return new IcebreakerResponse(input)
+  })
+}
